9f85f507048e2051de5b9f43bf50334afdbaf972
Deni Alpiana <Denzalv01@gmail.com>
2022-06-10 09:48:50 +0700

    secure with env file

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0d4L_AaJ0NX5TnBcVTqWlD4tKjKe_C_M",
-  authDomain: "nextjs-au.firebaseapp.com",
-  databaseURL: "https://nextjs-au-default-rtdb.firebaseio.com",
-  projectId: "nextjs-au",
-  storageBucket: "nextjs-au.appspot.com",
-  messagingSenderId: "28872013054",
-  appId: "1:28872013054:web:ff66fe3c89f61ebdb3e1af"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
